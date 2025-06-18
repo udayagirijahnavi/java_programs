679aef93444a55667f6823d141fd58eaf2b0ce73
udayagirijahnavi <udayagirijahnavi123@gmail.com>
2025-06-18 12:16:55 +0530

    Create linkedlist.java

---
+++ list/linkedlist.java
@@ -0,0 +1,21 @@
+// Java program to add elements to a LinkedList
+import java.util.LinkedList;
+
+public class solution {
+
+    // Main driver method
+    public static void main(String[] args) {
+        // Creating a LinkedList
+        LinkedList<String> li = new LinkedList<String>();
+
+        // Adding elements to the LinkedList using add() method
+        li.add("One");
+        li.add("Two");
+        li.add("Three");
+        li.add("Four");
+        li.add("Five");
+
+        // Printing the LinkedList
+        System.out.println(li);
+    }
+}
